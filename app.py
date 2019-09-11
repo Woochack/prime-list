0ca0150e13f3ca0fa4483bf703db6a8620443ef1
Adam Luczak <adam@Adams-MacBook-Pro.local>
2019-09-11 17:34:50 +0200

    added method for listing prime numbers in given range

--- app.py
+++ app.py
@@ -2,5 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/<int:num>')
+def get_list(num):
+    primes = []
+    for prime in range(1, num):
+        if prime > 1:
+            for i in range(2, prime):
+                if (prime % i) == 0:
+                    break
+            else:
+                primes.append(prime)
+    return {'Prime numbers in given range are as follows': primes}
+
+
 if __name__ == '__main__':
     app.run(debug=True)
