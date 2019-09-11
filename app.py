8bc99168586f708c5d8446ed018351bd1880721d
Adam Luczak <adam@Adams-MacBook-Pro.local>
2019-09-11 20:58:24 +0200

    added pagination to the list

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
+from flask_paginate import Pagination, get_page_args
+import math
 
 app = Flask(__name__)
 app.template_folder = ''
 
 
 @app.route('/<int:num>')
-def index(num):
-    primes = get_list(num)
-
+def paginate_list(num):
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    pagination_primes = get_primes(num, offset=offset, per_page=per_page)
+    total = math.ceil(len(get_list(num)))
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
     return render_template('index.html',
-                           primes=primes)
+                           primes=pagination_primes,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
+
+
+def get_primes(num, offset=1, per_page=10):
+    primes = get_list(num)
+    return primes[offset: offset + per_page]
 
 
 def get_list(num):
