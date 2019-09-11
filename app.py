644d4bce2534f9bff1d3d4e1285aca20b53ff2ec
Adam Luczak <adam@Adams-MacBook-Pro.local>
2019-09-11 21:59:06 +0200

    added exceptions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_paginate import Pagination, get_page_args
 import math
 
@@ -6,20 +6,29 @@ app = Flask(__name__)
 app.template_folder = ''
 
 
+@app.route('/')
+def get_foo():
+    raise InvalidUsage('Please input the range at the end of URL to print Prime Numbers for it, '
+                       'example: http://127.0.0.1:5000/1000', status_code=400)
+
+
 @app.route('/<int:num>')
 def paginate_list(num):
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-    pagination_primes = get_primes(num, offset=offset, per_page=per_page)
-    total = math.ceil(len(get_list(num)))
-    pagination = Pagination(page=page, per_page=per_page, total=total,
-                            css_framework='bootstrap4')
-    return render_template('index.html',
-                           primes=pagination_primes,
-                           page=page,
-                           per_page=per_page,
-                           pagination=pagination,
+    if num > 1:
+        page, per_page, offset = get_page_args(page_parameter='page',
+                                               per_page_parameter='per_page')
+        pagination_primes = get_primes(num, offset=offset, per_page=per_page)
+        total = math.ceil(len(get_list(num)))
+        pagination = Pagination(page=page, per_page=per_page, total=total,
+                                css_framework='bootstrap4')
+        return render_template('index.html',
+                               primes=pagination_primes,
+                               page=page,
+                               per_page=per_page,
+                               pagination=pagination,
                            )
+    else:
+        raise InvalidUsage('Invalid range, please input positive integer bigger than 1.', status_code=400)
 
 
 def get_primes(num, offset=1, per_page=10):
@@ -38,6 +47,29 @@ def get_list(num):
                 primes.append(prime)
 
     return primes
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 if __name__ == '__main__':
